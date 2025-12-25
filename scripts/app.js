c3c3176472e32f1c73b4a997d32c9d21aae761fd
Rishi Mishra <rishik7970@gmail.com>
2025-12-25 21:40:52 +0530

    added hero-section

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,17 @@
 console.log("E-Commerce Website Loaded");
+
+
+const hamburger = document.getElementById("hamburger");
+const nav = document.getElementById("navigation");
+
+hamburger.addEventListener("click", () => {
+    if (nav.style.display === "block") {
+        nav.style.display = "none";
+    } else {
+        nav.style.display = "block";
+    }
+})
+
+function goToProducts() {
+    window.location.href = "#"
+}
